8597be8567e9c46a65760cf83de2f829047bacec
62160120 <62160120@go.buu.ac.th>
2021-01-22 16:05:18 +0700

    add 05 and 03

--- exam1/04/end.js
+++ exam1/04/end.js
@@ -1,7 +1,5 @@
 const finalScore = document.querySelector('#finalScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 
-const MAX_HIGH_SCORES = 5
-
 finalScore.innerText = mostRecentScore
 
